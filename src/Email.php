115fdb87c0c3a5406880e7ede0a2a6256de92888
Emma Kim <eema.kim01@gmail.com>
2024-03-04 12:17:10 +0000

    create email class

---
+++ src/Email.php
@@ -0,0 +1,22 @@
+<?php
+
+readonly class Email
+{
+    public string $email;
+
+    public function __construct(string $email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception('Error: Invalid email');
+        }
+
+        $this->email = $email;
+    }
+
+    public function __toString(): string
+    {
+        return $this->email;
+    }
+}
+
+$email = new EmailAddress('ashton@test.com');
