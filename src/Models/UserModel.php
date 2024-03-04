0ff22fc833746d260506a980cad45660166fc8b7
Ashton Jeffrey <ashtonjeffrey@live.com>
2024-03-04 13:53:08 +0000

    created getUserByUsername method in UserModel

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -10,4 +10,27 @@ class UserModel
     {
         $this->db = $db;
     }
+
+    public function getUserByUsername(string $username): User | false
+    {
+      $query = $this->db->prepare('SELECT * FROM `users` WHERE `username`= :username');
+      $query->execute([
+          ':username' => $username
+      ]);
+
+      $data = $query->fetch();
+
+      return $this->hydrateSingleUser($data);
+    }
+
+    private function hydrateSingleUser($data): User|false
+    {
+        if ($data != false) {
+
+            return new User ($data['id'], $data['username'], $data['password'], $data['email']);
+        } else {
+            return false;
+        }
+
+    }
 }
