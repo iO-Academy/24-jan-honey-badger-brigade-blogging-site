421828383ba17a87717df03f139dab517682b767
Jenny <jenny@redsparkdigital.co.uk>
2024-03-04 14:09:42 +0000

    addBlogPost function created in blog model

--- src/Models/BlogModel.php
+++ src/Models/BlogModel.php
@@ -10,4 +10,16 @@ class BlogModel {
         $this->db = $db;
     }
 
+    public function addBlogPost(int $authorid, string $title, string $content, int $timestamp): bool
+    {
+        $query = $this->db->prepare
+        ('INSERT INTO `blogposts` (`authorid`, `title`, `content`, `posttime`)
+        VALUES (:authorid, :title, :content, :posttime)');
+        return $query->execute([
+            ':authorid' => $authorid,
+            ':title' => $title,
+            ':content' => $content,
+            ':posttime' => $timestamp,
+        ]);
+    }
 }
