b8bd40fce64c85605c40bf10f1346cb0802f6540
Elias Beyrouty <Elie.beyrouty@yahoo.co.uk>
2024-03-04 13:54:26 +0000

    created getAllPosts method under Blog Model

--- src/Models/BlogModel.php
+++ src/Models/BlogModel.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'src/Entities/Blog.php';
+require_once 'src/connectToDb.php';
 
 class BlogModel {
     private PDO $db;
@@ -8,6 +9,23 @@ class BlogModel {
     public function __construct(PDO $db)
     {
         $this->db = $db;
+    }
+
+    public function getAllPosts()
+    {
+        $query = $this->db->prepare('SELECT * FROM `blogposts` ORDER BY `posttime` DESC;');
+        $query ->execute();
+        $blog = $query->fetchAll();
+
+        $blogposts = [];
+        foreach ($blog as $blogposts)
+        {
+            $blogposts = new Blog($blogposts['id'], $blogposts['title'], $blogposts['content'], $blogposts['authorid'], $blogposts['posttime']);
+        }
+
+        return $blogposts;
+
+
     }
 
 }
