c3dce377f582329b31835c4fc8bbb3786ccd1904
Jenny <jenny@redsparkdigital.co.uk>
2024-03-06 13:15:37 +0000

    updated singlePost

--- singlePost.php
+++ singlePost.php
@@ -34,7 +34,7 @@ $blog = $blogModel->getBlogById($_GET['id'])
 <!--        <span class="px-3 py-2 bg bg-slate-200 inline-block mb-4 rounded-sm">Gaming</span>-->
         <div class="flex justify-between items-center flex-col md:flex-row mb-4">
             <h2 class="text-4xl"><?php echo $blog->title; ?></h2>
-<!--            <span class="text-xl">100 likes - 50 dislikes</span>-->
+            <span class="text-xl">100 likes - 50 dislikes</span>
         </div>
         <p class="text-2xl mb-10"><?php echo $blog->postTime . ' - By ' . $blog->id; ?></p>
         <p><?php echo $blog->content ?></p>
