1622ee56a504f4c00f42cbfa0b5d9a6283c6a954
Emma Kim <eema.kim01@gmail.com>
2024-03-06 15:20:14 +0000

    amending session start

--- addPost.php
+++ addPost.php
@@ -1,11 +1,10 @@
 <?php
     require_once 'src/Models/BlogModel.php';
     require_once 'src/connectToDb.php';
-
+session_start();
     if (!isset($_SESSION['userid'])) {
         header('Location: login.php');
     } else {
-        session_start();
         $authorid = $_SESSION['userid'];
     }
     $db = connectToDb();
