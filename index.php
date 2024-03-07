d499a82ffd81744542bfbf88a8c7f6c953b3bafa
Jenny <jenny@redsparkdigital.co.uk>
2024-03-07 19:33:58 +0000

    add category filter

--- index.php
+++ index.php
@@ -27,6 +27,37 @@ $blogs = $blogModel->getAllPosts();
     </div>
 </nav>
 
+<form class="container lg:w-1/2 gap-5 mx-auto mb-10 flex justify-between items-center flex-col lg:flex-row px-5 sm:px-0">
+    <div class=" w-full flex flex-col lg:flex-row gap-5">
+        <div>
+            <label for="category" class="text-lg block xl:inline">Filter by category:</label>
+            <select id="category" class="px-3 py-2 text-lg w-full xl:w-auto">
+                <option>All</option>
+                <option>News</option>
+                <option>Gaming</option>
+                <option>Films</option>
+                <option>TV</option>
+                <option>Science and Nature</option>
+            </select>
+        </div>
+
+        <div>
+            <label for="sort" class="text-lg block xl:inline">Sort by:</label>
+            <select id="sort" class="px-3 py-2 text-lg w-full xl:w-auto">
+                <option>Newest</option>
+                <option>Oldest</option>
+                <option>Most Liked</option>
+                <option>Most Disliked</option>
+            </select>
+        </div>
+    </div>
+
+
+    <input class="px-3 py-2 text-lg bg-indigo-400 hover:bg-indigo-700 hover:text-white transition inline-block rounded-sm" type="submit" value="Filter">
+
+</form>
+
+
 <section class="container lg:w-1/2 mx-auto flex flex-col gap-5">
 
     <?php
